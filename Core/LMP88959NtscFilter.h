88ac9a14bc0560f4d5273a19dcd52c4828285615
Persune <gumball2415@gmail.com>
2023-01-10 20:19:28 +0800

    Add NTSC-CRT library
    from https://github.com/LMP88959/NTSC-CRT

---
+++ Core/LMP88959NtscFilter.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "stdafx.h"
+#include "BaseVideoFilter.h"
+#include "../Utilities/crt.h"
+
+class Console;
+
+class LMP88959NtscFilter : public BaseVideoFilter
+{
+private:
+	nes_ntsc_setup_t _ntscSetup;
+	nes_ntsc_t _ntscData;
+	bool _keepVerticalRes = false;
+	bool _useExternalPalette = true;
+	uint8_t _palette[512 * 3];
+	uint32_t* _ntscBuffer;
+	bool _ntscBorder = true;
+
+	void GenerateArgbFrame(uint32_t *outputBuffer);
+
+protected:
+	void OnBeforeApplyFilter();
+
+public:
+	LMP88959NtscFilter(shared_ptr<Console> console);
+	virtual ~LMP88959NtscFilter();
+
+	virtual void ApplyFilter(uint16_t *ppuOutputBuffer);
+	virtual FrameInfo GetFrameInfo();
+};
